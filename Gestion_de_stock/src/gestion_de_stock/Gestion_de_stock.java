dca321b94cef2941c0b2d102d4b0e68b11b8da41
Aida <102191104+Ayda2234@users.noreply.github.com>
2023-11-18 17:59:13 +0100

    Add files via upload

---
+++ Gestion_de_stock/src/gestion_de_stock/Gestion_de_stock.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestion_de_stock;
+
+/**
+ *
+ * @author RAM COM
+ */
+public class Gestion_de_stock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Interface_principale log = new Interface_principale();
+        //log.show();
+        // TODO code application logic here
+    }
+
+    void show() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
